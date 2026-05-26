1b6806633632ae6eebb091f91e93d5e63c24567d
Denis <den.melnickov2001@yandex.ru>
2026-05-26 16:59:58 +0300

    feat: prod

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://zamenitpriprode.ru',
+    siteUrl: 'https://tips.samsmu.ru/logic-trainer/',
     generateRobotsTxt: true,
     sitemapSize: 7000,
 };
--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: 'zamenitpriprode.ru',
+                hostname: 'tips.samsmu.ru/logic-trainer/',
                 pathname: '/media/**',
             },
         ],
