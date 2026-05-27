40867a78df2ea155cfa5936a249a8377414270f7
Denis <den.melnickov2001@yandex.ru>
2026-05-27 18:00:50 +0400

    Для выкладки на клауд ру v5

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
     reactStrictMode: true,
     trailingSlash: true,
     basePath: '/logic-trainer',
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://172.26.4.9:8083/logic-trainer/api/:path*',
+            },
+        ];
+    },
     output: 'standalone',
     images: {
         remotePatterns: [
