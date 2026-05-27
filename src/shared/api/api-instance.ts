c568e833ee565a8e0acf4afe040e677a8879bceb
Denis <den.melnickov2001@yandex.ru>
2026-05-27 18:06:02 +0400

    Для выкладки на клауд ру v6

--- src/shared/api/api-instance.ts
+++ src/shared/api/api-instance.ts
@@ -18,7 +18,7 @@ export function getCookie(name: string): string | null {
 }
 
 export const apiInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "/api",
+    baseURL: "/logic-trainer/api",
     headers: {
         "Content-Type": "application/json",
     },
